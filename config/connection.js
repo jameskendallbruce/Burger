a9e859ace2044844a04a4d770f9a60d6cdfb673b
jameskendallbruce <jameskendallbruce@gmail.com>
2018-08-21 18:04:10 -0400

    Add files via upload

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,14 @@ var mysql = require("mysql");
 
 var connection
 if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else{
-  connections = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 8889,
     user: "root",
     password: "root",
-    database: "burger_db"
+    database: "m0b85542v73htil6"
   });
 };
 // Make connection.
