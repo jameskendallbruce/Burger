a78017c150c6739ea3825710b2dd9f23a01e6887
jameskendallbruce <jameskendallbruce@gmail.com>
2018-08-21 09:50:11 -0400

    Add files via upload

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection
+if (process.env.JAWSDB_URL){
+} else{
+  connections = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burger_db"
+  });
+};
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('BBQ Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Greek Goat Cheese Turkeyburger');
+INSERT INTO burgers (name) VALUES ('Classic Cheeseburger');
